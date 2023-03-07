3ce5f2a08770dab317a6e78a8f64a7a73b70ff90
ethan <e_posner@outlook.com>
2023-03-06 17:25:54 -0800

    Created simple curses chat interface

---
+++ documentation/INTERFACE.md
@@ -0,0 +1,7 @@
+# Terminal Interface
+
+Run with: `python socksy/curses_chat_window.py`
+
+## Controls
+- `:msg <message>`: Send new message in the channel
+- `:exit`: Quit the program and return to the terminal
---
+++ socksy/modules/chat_data.py
@@ -0,0 +1,56 @@
+from typing import Union
+import datetime as dt
+
+from dateutil.parser import parse
+
+class User:
+    name: str
+    created_at: dt.datetime
+    status: bool
+    
+    def __init__(self, name: str, created_at: dt.datetime, status: bool = False):
+        self.name = name
+        self.status = status
+        if isinstance(created_at, dt.datetime):
+            self.created_at = created_at
+        elif isinstance(created_at, str):
+            self.created_at = parse(created_at)
+        else:
+            raise ValueError("The given created_at value did not match a recognized type. Supply either a datetime string or object")
+        
+    def set_status(self, status: bool):
+        self.status = status
+        
+    @property
+    def online(self):
+        return self.status
+    
+    def __str__(self):
+        return self.name
+    
+    __repr__ = __str__
+
+
+class Message:
+    content: str
+    sender: User
+    sent_at: dt.datetime
+    
+    def __init__(self, content: str, sender: User, sent_at: Union[dt.datetime, str] = dt.datetime.now()):
+        self.content = content
+        self.sender = sender
+        if isinstance(sent_at, dt.datetime):
+            self.sent_at = sent_at
+        elif isinstance(sent_at, str):
+            self.sent_at = parse(sent_at)
+        else:
+            raise ValueError("The given sent_at value did not match a recognized type. Supply either a datetime string or object")
+
+    def date_str(self):
+        pass
+    
+    def time_str(self):
+        return self.sent_at.strftime("%H:%M")
+    
+    def datetime_str(self):
+        pass
