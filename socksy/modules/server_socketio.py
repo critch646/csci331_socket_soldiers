b8fbaf19122fdd08a0de88c977bdfacb9af187e4
Zeke Critchlow <critch646@gmail.com>
2023-03-21 19:49:15 -0700

    refactored socksy_server.py, breaking-out functions and objects into modules/server_socketio.py and modules/server_socket_handlers.py

---
+++ socksy/modules/server_socketio.py
@@ -0,0 +1,17 @@
+"""
+Creates and configures a Flask SocketIO server instance.
+
+@name: server_socketio
+"""
+
+from flask import Flask
+from flask_socketio import SocketIO
+
+# Initialize Flask and SocketIO (must be global)
+socksyServer = Flask(__name__)
+socksyServer.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(socksyServer)
+
+import eventlet
+# Monkey-patch Eventlet (must be global)
+eventlet.monkey_patch()
