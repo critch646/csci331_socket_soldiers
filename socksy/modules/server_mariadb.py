1f9b5ff8f28f632e215d9aec85df855625c71ecc
Brandon Stanton <brandonstanton@outlook.com>
2023-03-22 11:28:56 -0700

    created DatabaseConnection class in 'server_mariadb.py', and also:      + added a 'add_message()' method which adds a message to the Messages table on the database on a 'message' event server side (UNTESTED)      + added a 'add_user()' method which will add a new user to the Users table -- (this is not implemented anywhere in the main running of the server for safety purposes)
    created a env file which is used by the above DatabaseConnection class, this pulls from a (untracked) .env file which has all the credentials to log into the dolphin database in the csci dept. network
    
    added the necessary import to make the 'dolphin_db' object used in the __main__ code global, should be accessible wherever we need it-- but better to keep it to the 'server_socket_handlers.py' file for now

--- socksy/modules/server_mariadb.py
+++ socksy/modules/server_mariadb.py
@@ -1,20 +1,42 @@
 # Module Imports
 import mariadb
 import sys
+from dotenv import load_dotenv
 
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user="csci331a",
-        password="S2K4uv7Z",
-        host="marie.csci.viu.ca",
-        port=3306,
-        database="csci331a_socksy"
 
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+class DatabaseConnection:
 
-# Get Cursor
-cur = conn.cursor()
+    # prepared statements for inserting data
+    add_msg_stmt = "INSERT INTO Messages (userID, content, sentAt) VALUES (?,?,?)"
+    add_usr_stmt = "INSERT INTO Users (username, permissionLevel) VALUES (?,?)"
+
+    def __int__(self, envfile):
+
+        # load information from the ENV file
+        load_dotenv('../dolphin.env')
+
+        USERNAME = os.getenv('USERNAME')
+        PASSWORD = os.getenv('PASSWORD')
+        HOST = os.getenv('HOST')
+        DATABASE = os.getenv('DATABASE')
+
+        try:
+            self.conn = mariadb.connect(
+                user=USERNAME,
+                password=PASSWORD,
+                host=HOST,
+                database=DATABASE)
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+
+        # Get Cursor
+        self.cur = conn.cursor()
+
+    def add_message(self, user_id, content, sent_at):
+        """INSERT INTO Messages (userID, content, sentAt) VALUES (?,?,?)"""
+        self.cur.execute(self.add_msg_stmt, (user_id, content, sent_at))
+
+    def add_user(self, username, permission_level):
+        """INSERT INTO Users (username, permissionLevel) VALUES (?,?)"""
+        self.cur.execute(self.add_usr_stmt, (username, permission_level))
