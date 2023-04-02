2b7b144a9064ec7968fae09fe0e693fe83209ae5
Brandon Stanton <brandonstanton@outlook.com>
2023-04-01 18:13:58 -0700

    added comment for server_db_connect.py to describe what file is there for
    added 'known bugs' to main README.md to note the weird bell icons that show up in the messages sent by client application instances

--- socksy/modules/server_db_connect.py
+++ socksy/modules/server_db_connect.py
@@ -1,3 +1,6 @@
+"""
+creates a global variable for the database connection for use in the entire server application
+"""
 from .servermariadb import DatabaseConnection
 
 dolphin_db = DatabaseConnection()
